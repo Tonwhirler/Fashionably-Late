df40f5b5a392bc165cf9728972fe43f625b6f10c
Garrett Wilhelm <garrett.wilhelm@gmail.com>
2018-11-16 00:15:33 -0700

    Camera Follows Player
    Created an empty game object as player.child(0) and put the camera there. Camera rotates to always be behind the player, but it needs to be smoothed.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
+    [HideInInspector]
     public GameObject player;       //Public variable to store a reference to the player game object
 
-
-    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
-
+    //private float rotationSpeed = 2;
 
     public void SetPlayer(GameObject target)
     {
         player=target;
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
         if(player == null)return;
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+
+        transform.position = player.transform.GetChild(0).position;
+                //Slerp is smooth rotation, but target is messed up somehow
+                //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position), rotationSpeed * Time.deltaTime);
+        transform.LookAt(player.transform.position); //instant rotation, doesn't look good but works
     }
 }
